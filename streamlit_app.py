fc5a648e00ba80858b941e1e90dfc6126d429950
Prateek Singh <96137468+prateeksingh0106@users.noreply.github.com>
2023-04-17 12:50:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ Created on Fri Apr 14 13:27:50 2023
 @author: rohan
 """
 import streamlit as st
+import pandas
+import numpy
+import sklearn
+import matplotlib
 import pickle
 
 pickle_file_in = open("regressor.pkl","rb")
